31bda67f64fa202d7175a9b44b71d13a920e394e
antaga04 <antaga04@gmail.com>
2023-09-28 19:11:13 +0200

    resolve import

--- src/pages/Apod/Apod.jsx
+++ src/pages/Apod/Apod.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Apod.css'
 import Template from '../../components/Template/Template'
 import { NASA_URL, today } from '../../utils/data';
-import ApodModel from '../../components/APODModel/ApodModel';
+import ApodModel from '../../components/ApodModel/ApodModel';
 
 const Apod = () => {
   const APOD_URL = `${NASA_URL}planetary/apod?`;
