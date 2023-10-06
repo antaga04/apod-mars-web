a600ddcac1d280deee7e7e7c2f02e7b0ed4fc8c8
antaga04 <antaga04@gmail.com>
2023-10-06 13:22:16 +0200

    feat: add page notFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>Error 404</div>
-  )
-}
+    <main className="notfound">
+      <section className="black-hole">
+        <b></b>
+      </section>
+      <div className="error-message">
+        <h1>Error 404</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+      </div>
+    </main>
+  );
+};
 
-export default NotFound
+export default NotFound;
