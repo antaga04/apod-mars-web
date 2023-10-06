23f124470d420e2e10eabc0e2bbfc9558c5c0266
antaga04 <antaga04@gmail.com>
2023-10-06 15:17:59 +0200

    feat: fix msg color

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Message.css';
 
 const Message = ({ msg }) => {
-  return <div className="error-message flex-center">{msg}</div>;
+  return <div className="message flex-center"><p>{msg}</p></div>;
 };
 
 export default Message;
